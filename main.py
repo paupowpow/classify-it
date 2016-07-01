bbaf4f561f65fe241dce0e0ad0398b1f87a61822
TrangD <trang.d.wiredelta@gmail.com>
2016-07-01 14:31:34 +0200

    finished train()

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 import codecs
 import collections
+import math
 import os
 import os.path
 import time
 import re
 
 from vocabulary import Vocabulary
-
 
 class Main:
 
@@ -15,7 +15,7 @@ class Main:
 
     def __init__(self):
         train_results = self.__train(self.classes, self.concatenated_text_of_all_classes)
-        print('')
+        self.__classify_all_files(self.classes, train_results.prior, train_results.condprob)
 
     def __train(self, classes, concatenated_text_of_all_classes):
         prior = {}
@@ -26,7 +26,6 @@ class Main:
         concatenated_text_of_all_classes = self.__concatenate(concatenated_text_of_all_classes)
         for c in classes:
             t_ct = {}
-            condprob[c] = {}
             number_of_docs_in_c = self.__count_docs([c])
             prior[c] = number_of_docs_in_c/number_of_all_docs
             for t in vocabularies:
@@ -34,16 +33,41 @@ class Main:
 
             number_of_all_tokens_in_class = len(concatenated_text_of_all_classes[c].split())
             for t in vocabularies:
-                condprob[c][t] = (t_ct[t]+1)/(number_of_all_tokens_in_class + len(vocabularies))
+                if (t in condprob) == False:
+                    condprob[t] = {}
+                condprob[t][c] = (t_ct[t]+1)/(number_of_all_tokens_in_class + len(vocabularies))
         return self.__return_multiple_values(vocabularies, prior, condprob)
 
-    def __apply_multinomial_nb(self,):
-        print('')
+    def __classify_all_files(self, classes, prior, condprob):
+        for c in classes:
+            path = "data/" + c + '/test'
+
+            # for each doc in /train
+            for d in os.listdir(path):
+                if d.endswith(".txt"):
+                    result = self.__apply_multinomial_nb(d, path, classes, prior, condprob)
+                    print(d+': '+ result)
+
+    def __apply_multinomial_nb(self, d, path, classes, prior, condprob):
+        score = {}
+        tokens = self.__extract_tokens_from_doc(d, path)
+        for c in classes:
+            score[c] = math.log(prior[c])
+            for t in tokens:
+                if t in condprob:
+                    score[c] += math.log(condprob[t][c])
+        return max(score, key=score.get)
+
+    def __extract_tokens_from_doc(self, d, path):
+        doc_content = self.__read_doc(path, d)
+        doc_content = self.__prettify_string(doc_content)
+        tokens = doc_content.split()
+        return tokens
 
     def __extract_vocabularies_from_data(self, classes):
         vocabularies = set()
         for c in classes:
-            strings = self.__access_strings(c)
+            strings = self.__access_strings(c, '/train')
             vocabulary = Vocabulary(strings)
 
             curr_vocabulary = vocabulary.get_vocabulary()
@@ -64,25 +88,27 @@ class Main:
             file.write("%s " % item)
 
     # gets all content from all training docs from one class and stores it all into a list of strings.
-    def __access_strings(self, c):
+    def __access_strings(self, c, subfolder):
         strings = []
 
         # concatenate path
-        path = "data/" + c + "/train"
+        path = "data/" + c + subfolder
 
         # for each doc in /train
         for f in os.listdir(path):
             if f.endswith(".txt"):
-                path_to_file = path + "/" + f
-                open_file = codecs.open(path_to_file, "r", "utf-8")
-
-                string = open_file.read()
+                string = self.__read_doc(path, f)
                 strings.append(string)
-
-                open_file.close()
 
         self.concatenated_text_of_all_classes[c] = strings
         return strings
+
+    def __read_doc(self, path, filename):
+        path_to_file = path + "/" + filename
+        open_file = codecs.open(path_to_file, "r", "utf-8")
+        string = open_file.read()
+        open_file.close()
+        return string
 
     def __count_docs(self, classes):
         number_of_docs = 0
@@ -110,9 +136,9 @@ class Main:
     def __count_tokens_of_term(self, text, t):
         return text.count(' '+t+' ')
 
-    def __return_multiple_values(self, x, y, z):
-        results = collections.namedtuple('Results', ['x', 'y', 'z'])
-        results = results(x, y, z)
+    def __return_multiple_values(self, vocabularies, prior, condprob):
+        results = collections.namedtuple('Results', ['vocabularies', 'prior', 'condprob'])
+        results = results(vocabularies, prior, condprob)
         return results
 
 start_time = time.time()
